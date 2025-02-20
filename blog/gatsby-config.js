0eb0448fea7c64a3e0616c1f61345e0e71d6a97b
sahli-m <sahlim@gmatc.matc.edu>
2025-02-20 17:14:35 -0600

    Completed Activity 3

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -26,8 +26,8 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        spaceId: '${process.env.SPACE_ID}',
-        accessToken:'${process.env.ACCESS_TOKEN}' ,
+        spaceId: process.env.SPACE_ID,
+        accessToken:process.env.ACCESS_TOKEN ,
       },
     },
     `gatsby-plugin-image`,
@@ -38,6 +38,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
