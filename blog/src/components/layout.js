96f0beb5520b3954839a503823b56efba5637304
sahli-m <sahlim@gmatc.matc.edu>
2025-02-22 21:37:52 -0600

    Convert layout.js to use styled-components and theme

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -5,11 +5,23 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import styled, { ThemeProvider } from "styled-components";
+import { Gray } from "./themes/Gray";
+import Header from "./header";
+import "./layout.css";
 
-import Header from "./header"
-import "./layout.css"
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: var(--size-content);
+  padding: var(--size-gutter);
+`;
+
+const Footer = styled.footer`
+  margin-top: var(--space-5);
+  font-size: var(--font-sm);
+`;
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -20,32 +32,22 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+    <ThemeProvider theme={Gray}>
+      <>
+        <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+        <Content>
+          <main>{children}</main>
+          <Footer>
+            © {new Date().getFullYear()} &middot; Built with{' '}
+            <a href="https://www.gatsbyjs.com">Gatsby</a>
+          </Footer>
+        </Content>
+      </>
+    </ThemeProvider>
+  );
+};
 
-export default Layout
+export default Layout;
