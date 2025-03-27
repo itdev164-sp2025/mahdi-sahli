ae5549d2528ddc625312ba6791dea7a184973539
sahli-m <sahlim@gmatc.matc.edu>
2025-03-27 18:50:04 -0500

    Completed Activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -12,7 +12,7 @@ require("dotenv").config({
  */
 module.exports = {
   siteMetadata: {
-    title: ` Gatsby blog`,
+    title: ` My blog`,
     description: ``,
     author: `Mahdi Sahli`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
