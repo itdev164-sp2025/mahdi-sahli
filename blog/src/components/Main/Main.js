949c8e0f31a0937daef249603c53219f16240f8e
sahli-m <sahlim@gmatc.matc.edu>
2025-04-10 19:28:23 -0500

    Added Main, Section, and Footer components

---
+++ blog/src/components/Main/Main.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Box } from "rebass";
+
+const Main = (props) => <Box as="main" {...props} />;
+
+export default Main;
---
+++ blog/src/components/Section/Section.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Box } from "rebass";
+
+const section = (props) => <Box as="section" {...props} />;
+
+export default section;
